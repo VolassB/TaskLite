02cb5143f4a85edc6d1e3e5321f3c93d8d185735
VolassB <boh228pidor@gmail.com>
2026-01-19 20:36:25 +0500

    lesson 1

--- script.js
+++ script.js
@@ -25,3 +25,31 @@ for (let i = 0; i < task.length; i++) {
 
 console.log(searchTitle('Сделать кофе с молоком'));
 console.log(changesStatus('Сделать кофе с молоком'))
+
+for (let i = 0; i < task.length; i++) {
+    console.log(task[i].id);
+    console.log(task[i].title);
+}
+
+for (tas of task) {
+    console.log(tas);
+}
+
+let i = 0;
+let score = 0;
+while (i < task.length) {
+    if (task[i].status === 'Активна') {
+        score++;
+    }
+    if (task[i].status === 'Завершена') {
+        score++;
+    }
+    i++;
+}
+console.log(score)
+
+for (let i = 0; i < task.length; i++) {
+    if (task[i].status === 'Активна') {
+        console.log(task[i].title);
+    }
+}
